013ce5c781c873de21d51e233b3e5fff1d9f1c52
HovhannesTutyan <atutyanaz@gmail.com>
2022-10-26 20:17:09 +0400

    django chat with jqueri front

--- src_django/chat/views.py
+++ src_django/chat/views.py
@@ -1,7 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
 
 def index(request):
     return render(request, "just_chat/index.html")
 
+
 def room(request, room_name):
-    return render(request, "just_chat/room.html", {"room_name": room_name})
+    return render(request, "just_chat/room.html", {
+        "room_name_json": mark_safe(json.dumps(room_name)), 
+        "username": mark_safe(json.dumps(request.user.username)),
+        })
+    
